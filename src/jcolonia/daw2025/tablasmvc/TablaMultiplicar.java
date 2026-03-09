8b1c9e8eccd9f289cc8d30783e8487abd53706a8
Hernán José Higuero Ortega <hernanj.higort@educa.jcyl.es>
2026-03-09 12:14:23 +0100

    Creación de esqueleto clase TablaMultiplicar con documentación.

---
+++ src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
@@ -0,0 +1,60 @@
+package jcolonia.daw2025.tablasmvc;
+
+import java.util.List;
+
+/**
+ * Creación de tablas de multiplicar.
+ * Aplicación de texto usando tablas de multiplicar infantiles clásicas.
+ * 
+ * @author <a href="hernanj.higort@educa.jcyl.es">Hernán José Higuero Ortega</a>
+ * @version 1.0 (20260309)
+ */
+public class TablaMultiplicar {
+	/**
+	 * Número de la tabla de multiplicar.
+	 */
+	private int número;
+	
+	/**
+	 * Lista generica de cadenas de texto.
+	 */
+	private List<String> listaTextos;
+	
+	/**
+	 * Inicialización de variable sy creación de objetos.
+	 * @param número
+	 */
+	public TablaMultiplicar(int número) {
+		
+	}
+	
+	/**
+	 * Genera la tabla de multiplicar.
+	 */
+	public void generarTabla() {
+		
+	}
+	
+	/**
+	 * Facilita la lista con la tabla de multiplciar a la clase {@link ExportacionArchivo}.
+	 * @return null.
+	 */
+	public List<String> toListaExportacion() {
+		return null;
+	}
+	
+	/**
+	 * Envia la lista con la tabla de multiplicar a pantalla.
+	 * @return null.
+	 */
+	public List<String> toListaPantalla() {
+		return null;
+	}
+	
+	/**
+	 * guarda la tabla de multiplicar en una cadena de texto.
+	 */
+	public String toString() {
+		return null;
+	}
+}

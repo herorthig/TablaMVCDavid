0ce49635e8f194d826af15cc7c135a8179fded1c
Hernán José Higuero Ortega <hernanj.higort@educa.jcyl.es>
2026-03-10 12:30:42 +0100

    Creación de esqueleto clase ExportacionArchivo con documentación.

---
+++ src/jcolonia/daw2025/tablasmvc/ExportacionArchivo.java
@@ -0,0 +1,36 @@
+package jcolonia.daw2025.tablasmvc;
+
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * Exportación de la tabla de multiplicar a un archivo de texto.
+ * Aplicación de texto usando tablas de multiplicar infantiles clásicas.
+ * 
+ * @author <a href="hernanj.higort@educa.jcyl.es">Hernán José Higuero Ortega</a>
+ * @version 1.0 (20260310)
+ */
+public class ExportacionArchivo {
+
+	/**
+	 * Ruta completa del archivo donde guardaremos la tabla de multiplicar.
+	 */
+	private Path refArchivos;
+	
+	/**
+	 * Exporta la tabla de multiplicar a un archivo de texto.
+	 * @param rutaArchivo
+	 */
+	public ExportacionArchivo(String rutaArchivo) {
+		
+	}
+	
+	/**
+	 * Guarda la tabla de multiplicar en un archivo de texto.
+	 * @param contenidos
+	 */
+	public void guardar(List<String> contenidos) {
+		
+	}
+	
+}

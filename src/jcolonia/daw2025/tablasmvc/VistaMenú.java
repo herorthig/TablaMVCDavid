e04511f4bab8fd2d67ab96ca64c5ab4a966eef86
Hernán José Higuero Ortega <hernanj.higort@educa.jcyl.es>
2026-03-11 09:05:11 +0100

    Creación de esqueleto clase VistaMenú con documentación.

---
+++ src/jcolonia/daw2025/tablasmvc/VistaMenú.java
@@ -0,0 +1,60 @@
+package jcolonia.daw2025.tablasmvc;
+
+import java.util.List;
+
+/**
+ * Vista del menú del programa.
+ * Aplicación de texto usando tablas de multiplicar infantiles clásicas.
+ * 
+ * @author <a href="hernanj.higort@educa.jcyl.es">Hernán José Higuero Ortega</a>
+ * @version 1.1 (20260311)
+ */
+public class VistaMenú extends VistaGeneral {
+	
+	/**
+	 * Titulo del menú.
+	 */
+	private String título;
+	
+	/**
+	 * Opciones del menú.
+	 */
+	private List<String> opciones;
+	
+	/**
+	 * Inicialización de titulo y creación de nuevo ArrayList de opciones.
+	 * 
+	 * @param título que se ponga
+	 * @param opciones de la lista
+	 */
+	public VistaMenú(String título, List<String> opciones) {
+		
+	}
+	
+	/**
+	 * Muestra el titulo del menú.
+	 */
+	public void mostrarTítulo() {
+		
+	}
+	
+	/**
+	 * Muestra las opciones del menú, una tras otra en orden.
+	 */
+	public void mostrarOpciones() {
+		
+	}
+	
+	/**
+	 * Pide un numeor al usuario.
+	 * 
+	 * @return opción elegida por el usuario.
+	 */
+	public int pedirOpción() {
+		int opción;
+		
+		opción = 0;
+		
+		return opción;
+	}
+}

98524bc4d4c1758fa42746220c1593dfa7a696c1
Hernán José Higuero Ortega <hernanj.higort@educa.jcyl.es>
2026-03-12 09:32:20 +0100

    Arreglos menores en clase LanzadorTablaMultiplicar.

--- src/jcolonia/daw2025/tablasmvc/LanzadorTablaMultiplicar.java
+++ src/jcolonia/daw2025/tablasmvc/LanzadorTablaMultiplicar.java
@@ -1,18 +1,22 @@
 package jcolonia.daw2025.tablasmvc;
 /**
-* Lanzador de aplicación de consola de texto con menús. Aplicación
-* de texto usando tablas de multiplicar infantiles clásicas. 
-*/
+ * Lanzador de aplicación de consola de texto con menús. Aplicación
+ * de texto usando tablas de multiplicar infantiles clásicas. 
+ * 
+ * @author <a href= "mailto:dmartin.jcolonia@gmail.com">David H. Martín</a>
+ * @version 1.1 (20260312)
+ */
 public class LanzadorTablaMultiplicar {
+	
 	/**
-	* Inicia el menú principal del programa.
-	* @param argumentos opciones de ejecución -no se usan-
-	*/
+	 * Inicia el menú principal del programa.
+	 * @param argumentos opciones de ejecución -no se usan-
+	 */
 	public static void main (String[] argumentos){
 		ControlTablaMultiplicar programa;
 		
-		programa = new ControlTablaMultiplicar()
+		programa = new ControlTablaMultiplicar();
 		
-		programa.bucleprincipal();
+		programa.buclePrincipal();
 	}
 }
